c54d5f52e453ca215879bfc3607c99dea1325d1d
basithallied <basithallied@gmail.com>
2025-12-31 02:42:23 +0530

    vb

--- zcl_sale/models/res_company.py
+++ zcl_sale/models/res_company.py
@@ -3,4 +3,4 @@ from odoo import fields, models
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    auto_validate_delivery = fields.Boolean(string="Auto Validate Delivery", default=False)
+    # auto_validate_delivery = fields.Boolean(string="Auto Validate Delivery", default=False)
--- zcl_sale/models/res_config_settings.py
+++ zcl_sale/models/res_config_settings.py
@@ -3,4 +3,4 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    auto_validate_delivery = fields.Boolean(related='company_id.auto_validate_delivery', readonly=False)
+    # auto_validate_delivery = fields.Boolean(related='company_id.auto_validate_delivery', readonly=False)
