1cb229ba9da30e62e2559ed4fe602bd089023c9a
basithallied <basithallied@gmail.com>
2026-01-01 07:04:36 +0530

    version

--- zcl_journal_import/__manifest__.py
+++ zcl_journal_import/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'ZCL Journal Import',
-    'version': '17.0.1.0.0',
+    'version': '18.0.1.0.0',
     'category': 'Accounting',
     'summary': 'Import Journal Entries from Excel',
     'description': """
